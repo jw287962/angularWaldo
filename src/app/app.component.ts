0fc24778969fb99fbec507a613860f2ac0d68eb2
Jason <jasonwong287962@gmail.com>
2023-06-16 03:14:00 -0500

    store location ratio for characters ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { ImagesComponent } from './images/images.component';
-import { Images } from './images/images';
+import { Coordinates, Images, Character } from './images/images';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,11 +12,20 @@ export class AppComponent {
   imageList: Images[] = [
     {
       url: '../assets/WhereisWaldo.png',
-      name: 'Waldo',
+      name: 'Where is Waldo',
+      characters: [
+        new Character('Odlaw', [
+          { x: 282 / 1200, y: 424 / 900 },
+          { x: 282 / 1200, y: 495 / 900 },
+          { x: 304 / 1200, y: 424 / 900 },
+          { x: 304 / 1200, y: 495 / 900 },
+        ]),
+      ],
     },
     {
       url: '../assets/WhereisPikachu.jpeg',
-      name: 'Pikachu',
+      name: 'Where is Pikachu',
+      characters: [],
     },
   ];
 }
--- src/app/images/images.ts
+++ src/app/images/images.ts
@@ -1,4 +1,31 @@
 export interface Images {
   url: string;
   name: string;
+  characters: Character[];
 }
+
+export interface Coordinates {
+  x: number;
+  y: number;
+}
+// (name: string, coordinates: Coordinates[])
+export class Character {
+  name: string;
+  coordinates: Coordinates[];
+
+  constructor(name: string, coordinates: Coordinates[]) {
+    this.name = name;
+    this.coordinates = coordinates;
+  }
+
+  getCoordinates(): Coordinates[] {
+    return this.coordinates;
+  }
+  getName(): string {
+    return this.name;
+  }
+}
+// export function Coordinate(coords: Coordinates) {
+//   coords = coords;
+//   return coords;
+// }
