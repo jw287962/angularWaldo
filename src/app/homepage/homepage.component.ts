bcdb54b0b4986896d0feaee64631e79edd8190c2
Jason <jasonwong287962@gmail.com>
2023-06-21 20:28:24 -0500

    fix minor bug on scroll, nav is transparent

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -8,7 +8,7 @@ import { CommonModule } from '@angular/common';
 import { Images } from '../images/images';
 import { imageList } from '../imageLists';
 import { ImagesComponent } from '../images/images.component';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
 import { ImageStateService } from '../services/image-state.service';
 
 // import { AppRoutingModule } from '../app-routing.module';
@@ -22,16 +22,26 @@ import { ImageStateService } from '../services/image-state.service';
   encapsulation: ViewEncapsulation.None,
 })
 export class HomepageComponent {
-  constructor(private _imageStateService: ImageStateService) {}
+  constructor(
+    private _imageStateService: ImageStateService,
+    private router: Router
+  ) {}
   @Input() image!: Images;
   imageList = imageList;
+  startingRound = false;
 
   // onSelectImage(e: Event) {
   //   e.preventDefault();
   //   this.imageStateService.setIndex(0);
   // }'
-  setIndex(value: number): void {
-    this._imageStateService.setIndex(value);
+  processStartingGame(e: Event): void {
+    e.preventDefault();
+
+    this._imageStateService.setIndex(this.getIndex());
+    this.router.navigate(['searchImager']);
+  }
+  setIndex(val: number): void {
+    this._imageStateService.setIndex(val);
   }
   getIndex(): number {
     return this._imageStateService.getIndex();
